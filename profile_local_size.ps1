1666698222b350ae0be2afea586a850ff322fac8
Rimarenko <gennadij.rimarenko@gmail.com>
2019-02-07 16:02:37 +0200

    Add 2 scripts

--- profile_local_size.ps1
+++ profile_local_size.ps1
@@ -1,16 +1,12 @@
-$ComputerName = "CompName" #Name computer
-Invoke-Command $ComputerName -ErrorAction Stop -ScriptBlock {
-
-$Exclusions = ("Administrator", "Default", "Public", "ADMINI~1")
+﻿$Exclusions = ("Administrator", "Default", "Public", "ADMINI~1")
 $Profiles = Get-ChildItem -Path $env:SystemDrive"\Users" | Where-Object { $_ -notin $Exclusions }
 $AllProfiles = @()
 foreach ($Profile in $Profiles) {
 	$object = New-Object -TypeName System.Management.Automation.PSObject
-    $FolderSizes = "{0:N3}" -f ((Get-ChildItem $Profile.FullName -Recurse -File -Force -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue).Sum  / 1GB) + " GB"
+    $FolderSizes = "{0:N3} GB" -f ((Get-ChildItem $Profile.FullName -Recurse -File -Force -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum ).Sum /1Gb)
     $object | Add-Member -MemberType NoteProperty -Name ComputerName -Value $env:COMPUTERNAME.ToUpper()
 	$object | Add-Member -MemberType NoteProperty -Name Profile -Value $Profile
 	$Object | Add-Member -MemberType NoteProperty -Name Size -Value $FolderSizes
 	$AllProfiles += $object
-}
-write $AllProfiles | ft -AutoSize
-}
+ }
+ $AllProfiles | ft -AutoSize
---
+++ profile_remote_size.ps1
@@ -0,0 +1,15 @@
+$ComputerName = "test" #Name computer
+Invoke-Command $ComputerName -ErrorAction Stop -ScriptBlock {
+$Exclusions = ("Administrator", "Default", "Public", "ADMINI~1", "admin")
+$Profiles = Get-ChildItem -Path $env:SystemDrive"\Users" | Where-Object { $_ -notin $Exclusions }
+$AllProfiles = @()
+foreach ($Profile in $Profiles) {
+	$object = New-Object -TypeName System.Management.Automation.PSObject
+    $FolderSizes = "{0:N3} GB" -f ((Get-ChildItem $Profile.FullName -Recurse -File -Force -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum ).Sum /1Gb)
+    $object | Add-Member -MemberType NoteProperty -Name ComputerName -Value $env:COMPUTERNAME.ToUpper()
+	$object | Add-Member -MemberType NoteProperty -Name Profile -Value $Profile
+	$Object | Add-Member -MemberType NoteProperty -Name Size -Value $FolderSizes
+	$AllProfiles += $object
+ }
+ $AllProfiles | ft -AutoSize
+}
